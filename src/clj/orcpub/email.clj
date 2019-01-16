40e38684dcb185a26ed51355f1a4ff45d74a84df
John Liss <john@lissproductions.com>
2019-01-16 14:31:13 -0700

    Updated send email addresses

--- src/clj/orcpub/email.clj
+++ src/clj/orcpub/email.clj
@@ -32,7 +32,7 @@
 
 (defn send-verification-email [base-url {:keys [email username first-and-last-name]} verification-key]
   (postal/send-message (email-cfg)
-                       {:from "OrcPub Team <no-reply@orcpub.com>"
+                       {:from "OrcPub Team <no-reply@dungeonmastersvault.com>"
                         :to email
                         :subject "OrcPub Email Verification"
                         :body (verification-email
@@ -65,7 +65,7 @@
 
 (defn send-reset-email [base-url {:keys [email username first-and-last-name]} reset-key]
   (postal/send-message (email-cfg)
-                       {:from "OrcPub Team <no-reply@orcpub.com>"
+                       {:from "OrcPub Team <no-reply@dungeonmastersvault.com>"
                         :to email
                         :subject "OrcPub Password Reset"
                         :body (reset-password-email
